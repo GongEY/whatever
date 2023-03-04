4acf1031b3e3ab7ce36ec3a8df0dbc2bd6fee565
eunyeong <tbskdnwj528@yonsei.ac.kr>
2023-03-04 22:55:48 +0900

    new algorithm

--- Python3_Algorithm/main9.py
+++ Python3_Algorithm/main9.py
@@ -1,92 +1,41 @@
-def hello(miro, x, y, r, c, k, depth, tmpStr, answer):
-    # print(k, depth)
-    # print(x, y)
-    # print(tmpStr)
-    # print(answer)
-    # answer.sort()
-    # print(answer)
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+
+dx = [1, 0, 0, -1]
+dy = [0, -1, 1, 0]
+dAlpha = ['d', 'l', 'r', 'u']
+answer = 'z'
+
+def isValid(nx, ny, n, m):
+    return ny > 0 and ny <= m and nx > 0 and nx <= n
+
+def hello(n, m, x, y, r, c, k, depth, tmpStr):
+    global answer
+    if k < depth + abs(x-r) + abs(y-c):
+        return
+
     if x == r and y == c and k == depth:
-        return True
-    elif k == depth:
-        return False
-    else:
-        tmpStr.append("l")
-        if y - 1 > 0:
-            tmp = hello(miro, x, y - 1, r, c, k, depth + 1, tmpStr, answer)
-        else:
-            tmp = False
-        if not tmp:
-            tmpStr.pop()
-        else:
-            answer.append(''.join(tmpStr))
-            tmpStr.pop()
-
-        # print("l end!!")
-        tmpStr.append("r")
-        if y + 1 <= len(miro):
-            tmp = hello(miro, x, y + 1, r, c, k, depth + 1, tmpStr, answer)
-        else:
-            tmp = False
-        if not tmp:
-            tmpStr.pop()
-        else:
-            answer.append(''.join(tmpStr))
-            tmpStr.pop()
-
-        # print("r end!!")
-        tmpStr.append("u")
-        if x - 1 > 0:
-            tmp = hello(miro, x - 1, y, r, c, k, depth + 1, tmpStr, answer)
-        else:
-            tmp = False
-        if not tmp:
-            tmpStr.pop()
-        else:
-            answer.append(''.join(tmpStr))
-            tmpStr.pop()
-
-        # print("u end!!")
-        tmpStr.append("d")
-        if x + 1 <= len(miro[0]):
-            tmp = hello(miro, x + 1, y, r, c, k, depth + 1, tmpStr, answer)
-        else:
-            tmp = False
-        if not tmp:
-            tmpStr.pop()
-        else:
-            answer.append(''.join(tmpStr))
-            tmpStr.pop()
-
-        # print("everything end!!")
-        # print(tmpStr)
-        if len(tmpStr) == 0:
-            return answer
-        else:
-            return False
-
+        answer = tmpStr
+        return
+    for i in range(4):
+        if isValid(x+dx[i], y+dy[i], n, m) and tmpStr < answer:
+            hello(n, m, x + dx[i], y + dy[i], r, c, k, depth + 1, tmpStr + dAlpha[i])
 
 def solution(n, m, x, y, r, c, k):
-    answer = []
-    miro = [["." for _ in range(n)] for _ in range(m)]
-    miro[x - 1][y - 1] = "S"
-    miro[r - 1][c - 1] = "E"
-    depth = 0
-    tmpStr = []
-    # print(len(miro[0]), len(miro))
-    answer = hello(miro, x, y, r, c, k, depth, tmpStr, answer)
-    answer.sort()
-    # print(answer)
-    if len(answer) == 0:
+    dist = abs(x-r) + abs(y -c)
+    if dist > k or (k-dist) % 2 == 1:
         return "impossible"
-    else:
-        return answer[0]
+    hello(n, m, x, y, r, c, k, 0, "")
+
+    return answer
 
 if __name__ == '__main__':
     n = 3
-    m = 3
-    x = 1
-    y = 2
+    m = 4
+    x = 2
+    y = 3
     r = 3
-    c = 3
-    k = 4
+    c = 1
+    k = 5
     print(solution(n, m, x, y, r, c, k))
